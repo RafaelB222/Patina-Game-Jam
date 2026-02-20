f91b79b62b7bf52f2636921728ec1c2b0b192e3c
Uziel-8 <blade.ruger@gmail.com>
2026-02-20 22:10:39 +1030

    raf if a disgust sadist for watching me undergo this

--- Level/level_1.gd
+++ Level/level_1.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var level_center: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	PhysicsManager.use_center_gravity = false
+	PhysicsManager.use_center_gravity = true
 	
 	PhysicsManager.level_center = level_center
 
